5b629ee1e088813f7cb0adce18d608ab2db8ee6a
unknown <nachosabate1@gmail.com>
2021-02-16 18:45:30 -0300

    página de inicio

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/logreg', (req,res)=>{
+    res.sendFile(__dirname + '/views/logreg.html');
+});
