07afd9174db5fb2674213e4392f767bba805bdef
mmartella7744 <mati.martella@gmail.com>
2021-02-20 18:39:22 -0300

    footer y estilosFooter

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 4000, ()=>{
-    console.log('Servidor funcionando');
-});
-
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/logreg', (req,res)=>{
     res.sendFile(__dirname + '/views/logreg.html');
+});
+
+app.get('/footer', (req,res)=>{
+    res.sendFile(__dirname + '/views/footer.html');
+});
+
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('Servidor funcionando');
 });
